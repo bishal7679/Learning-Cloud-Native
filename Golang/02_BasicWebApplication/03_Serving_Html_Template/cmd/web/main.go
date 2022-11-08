88ff61c85545cac228cd366c77fcfc68c6b2d47d
Bishal Das <bishalhnj127@gmail.com>
2022-11-08 23:03:44 +0530

    updated golang

---
+++ Golang/02_BasicWebApplication/03_Serving_Html_Template/cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/bishal7679/Serving-html-template/pkg/handlers"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
---
+++ Golang/02_BasicWebApplication/03_Serving_Html_Template/go.mod
@@ -0,0 +1,3 @@
+module github.com/bishal7679/Serving-html-template
+
+go 1.18
