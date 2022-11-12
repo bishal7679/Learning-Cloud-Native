79981ca8354f8bcd84ce6bf80bf741702f3a8718
Bishal Das <bishalhnj127@gmail.com>
2022-11-12 22:59:47 +0530

    updated golang

---
+++ Golang/02_BasicWebApplication/03_Serving_Html_Template/cmd/web/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/bishal7679/Serving-html-template/pkg/config"
+	"github.com/bishal7679/Serving-html-template/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(a *config.AppConfig) http.Handler {
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
