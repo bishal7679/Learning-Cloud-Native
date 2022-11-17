3f990a90ad3a5b9871a49dff6dd205185889f1a3
Bishal das <bishalhnj127@gmail.com>
2022-11-17 11:54:38 -0500

    updated

--- Golang/02_BasicWebApplication/03_Serving_Html_Template/cmd/web/routes.go
+++ Golang/02_BasicWebApplication/03_Serving_Html_Template/cmd/web/routes.go
@@ -17,6 +17,7 @@ func routes(a *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
